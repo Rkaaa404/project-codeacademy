d8607aaecd4e91aa42ef59f45077602caf2a1908
Rkaaa404 <raykapranandita@gmail.com>
2024-11-04 18:21:56 +0700

    Menambahkan fitur pembulatan
    Fitur pembulatan pada dua digit terakhir bila digitnya bukan 0, untuk mempermudah perhitungan tagihan

--- Tip Calculator/tipcalc.py
+++ Tip Calculator/tipcalc.py
@@ -1,13 +1,22 @@
+#import library
+import math
+
 print("Selamat datang di kalkulator tip, silahkan isi data data ini:")
 # Meminta input user
 meal = int(input("Harga Makanan: Rp."))
 tip = int(input("Tip yang diberikan (dalam %): "))
 # Proses perhitungan
-mealTax = meal * 0.11
+mealTax = math.ceil(meal * 0.11)
 mealTaxed = meal + (mealTax)
-mealTip = mealTaxed * tip / 100
+mealTip = math.ceil(mealTaxed * tip / 100)
 mealTotal = mealTaxed + mealTip
+mealTotalRounded = math.ceil(mealTotal / 100) * 100
+mealRounding= mealTotalRounded - mealTotal
 # Print hasil 
 print("*******************************")
-print(f"Harga Makanan : Rp.{meal} \nPajak(PPN 11%) : Rp. {mealTax} \nTip({tip}%) : Rp.{mealTip} \nTotal : Rp.{mealTotal}")
+print(f"Harga Makanan : Rp.{meal} \nPajak(PPN 11%) : Rp. {mealTax} \nTip({tip}%) : Rp.{mealTip}")
+#Kondisi bila perlu pembulatan
+print(f"Total Sebelum Pembulatan: Rp.{mealTotal}")
+if mealRounding != 0:
+    print(f"Pembulatan : Rp.{mealRounding} \nTotal Setelah Pembulatan: Rp.{mealTotalRounded}")
 print("*******************************")
